5dbca2280042ec7fc65e921ead75ef1a79fb9ec3
Larry Battle <larry.battle.tech@gmail.com>
2012-11-05 17:19:41 -0600

    Update test/js/level3-tests.js

--- test/js/level3-tests.js
+++ test/js/level3-tests.js
@@ -22,11 +22,6 @@
 * @date 10-28-2012
 */
 
-test("test getCummulativeSumArray()", function () {
-	var fn = getCummulativeSumArray;
-	deepEqual(fn([]), [NaN]);
-	deepEqual(fn([1, 2, 3]), [1, 3, 6]);
-});
 test("test getObjectFromArray()", function () {
 	var fn = getObjectFromArray;
 	deepEqual(fn([1, 2, 3]), {
@@ -35,52 +30,73 @@ test("test getObjectFromArray()", function () {
 		3 : 2
 	});
 });
-test("test getSumArrThatFormValue()", function () {
-	var fn = function (val, nums) {
-		var cSums = getCummulativeSumArray(nums);
-		return getSumArrThatFormValue(val, nums, cSums);
+test("test NumModule constructor", function(){
+	var fn = function(a,b){
+		return new NumModule(a,b).toString();
 	};
-	deepEqual(fn(40, [1, 2, 3]), []);
-	deepEqual(fn(4, [1, 2, 3]), [[3, 1]]);
-	deepEqual(fn(6, [1, 2, 3, 4, 5]), [[5, 1], [4, 2], [3, 2, 1]]);
+	equal( fn(3), "NumModule(value=3)" );
+	equal( fn(3, [[1,2]]), "NumModule(value=3, subset((1,2)))" );
+	equal( fn(5, [[1,4],[2,3]]), "NumModule(value=5, subset((1,4),(2,3)))" );
 });
-test("test getAllSumArrThatFormValues()", function () {
-	var fn = getAllSumArrThatFormValues;
-	deepEqual(fn([1, 2, 3, 40]), {
-		3 : [[2, 1]],
-		"length": 1
-	});
-	deepEqual(fn([1, 2, 3, 4]), {
-		4 : [[3, 1]],
-		3 : [[2, 1]],
-		"length":2
-	});
-	deepEqual(fn([1, 2, 3, 4, 5, 6]), {
-		6 : [
-			[5, 1],
-			[4, 2],
-			[3, 2, 1]
-		],
-		5 : [
-			[4, 1],
-			[3, 2]
-		],
-		4 : [
-			[3, 1]
-		],
-		3 : [
-			[2, 1]
-		],
-		"length":7
-	});
+test("test NumModule.prototype.addSubset()", function(){
+	var fn = function(a,b,c){
+		return new NumModule(a,b).addSubset(c).toString();
+	};
+	equal( fn(5, [[1,4],[2,3]]), "NumModule(value=5, subset((1,4),(2,3)))" );
+	equal( fn(5, [[1,4],[2,3]], [2,3]), "NumModule(value=5, subset((1,4),(2,3)))" );
+	equal( fn(7, [[3,4],[1,6]], [2,5]), "NumModule(value=7, subset((3,4),(1,6),(2,5)))" );
 });
+test("test Collection constructor", function(){
+	var fn = function(a,b){
+		return new Collection(a).addNumber(b).toString();
+	};
+	equal(fn(0), "Collection(maxValue: 0, length:0)");
+	equal(fn(3), "Collection(maxValue: 3, length:0)");
+});
+test("test Collection.prototype.getTotalSubsetCount()", function(){
+	var a = new Collection(3, [1,2,3]);
+	equal( a.getTotalSubsetCount(), 0);
+	a.getNumber(3).addSubset([1,2]);
+	equal( a.getTotalSubsetCount(), 1);
+});
+test("test Collection.prototype.getNumber()", function(){
+	var fn = function(a,b,c){
+		return new Collection(a).addNumber(b).getNumber(c);
+	};
+	equal(fn(3,2,2).toString(), "NumModule(value=2)");
+	equal(fn(3,2,10), undefined);
+});
+test("test SubsetFinder constructor", function(){
+	var a = new SubsetFinder([1,2,3]);
+	
+	equal( a.toString(), "SubsetFinder(numSet: [1,2,3], queue: [1,2,3], collection: [length=3])" );
+	
+	equal( a.collection.toString(), 
+		"Collection(maxValue: 3, length:3, nums: [NumModule(value=1); NumModule(value=2); NumModule(value=3)])");
+});
+
+test("test SubsetFinder.prototype.addValueToNumSet()", function(){
+	var a = new SubsetFinder([1,2,3]);
+	a.addValueToNumSet(2,[2]);
+	equal(a.toString(), "SubsetFinder(numSet: [1,2,3], queue: [1,2,3], collection: [length=3])");
+	
+	a = new SubsetFinder([1,2,3]);
+	a.addValueToNumSet(1,[1]);
+	equal(a.toString(), "SubsetFinder(numSet: [1,2,3], queue: [1,2,3], collection: [length=3])");
+});
+/**
+* GUI Controls
+*/
 var inputEl = document.getElementById("input");
 inputEl.onkeyup = function(){
 	var nums = document.getElementById("input").value.match(/\d+/g),
-		result = getAllSumArrThatFormValues(nums),
-		output = JSON.stringify( result, null, 2 );
+		a = new SubsetFinder(nums),
+		result = a.solve().getCollectionSubsetCount(),
+		output = a.toString();
 		
-	document.getElementById("answer").value = result.length;
+	output += "\r\n############\r\n a.collection.toString() = \r\n" +  a.collection.toString();
+	
+	document.getElementById("answer").value = result;
 	document.getElementById("output").innerHTML = output;
 };
 inputEl.onkeyup();
